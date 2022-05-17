8042043512601b3101c5bf2dc84054541ef4c006
simple123w <104978124+simple123w@users.noreply.github.com>
2022-05-17 15:04:44 +0500

    salom
    alik

---
+++ .vscode/valmari/script.js
@@ -0,0 +1,128 @@
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click",
+	function () {
+		burger.classList.toggle("active");
+		menu.classList.toggle("active")
+	});
+	document.querySelector("#c1").style="background: rgb(0, 195, 255);"
+		document.querySelector("#c2").style="background: white;"
+		document.querySelector("#c3").style="background: white;"
+		document.querySelector("#c4").style="background: white;"
+	function btn1(){
+		document.querySelector("#c1").style="background: rgb(0, 195, 255);"
+		document.querySelector("#c2").style="background: white;"
+		document.querySelector("#c3").style="background: white;"
+		document.querySelector("#c4").style="background: white;"
+		document.querySelector('.card_text').innerHTML=` 
+		 <div class="card_text" id="text1">
+		<h2 class="title">A passion for putting patients first.</h2>
+	 <div class="suptitle">
+	   <div class="second">
+		   <div class="cub" ><div class="span"></div> A Passion for Healing</div>
+		   <div class="cub" ><div class="span"></div> All our best</div>
+		   <div class="cub" ><div class="span"></div> A Legacy of Excellence</div>
+	   </div>
+	   <div class="first">
+		   <div class="cub" ><div class="span"></div> 5-Star Care</div>
+		   <div class="cub" ><div class="span"></div> Believe in Us</div>
+		   <div class="cub" ><div class="span"></div> Always Caring</div>
+	   </div>
+   
+   </div>
+   <div>
+	   <p class="card_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error temporibus voluptates alias pariatur minima,
+			fugiat suscipit eum quaerat tempora earum doloribus qui itaque deleniti ex blanditiis assumenda vel! Sed, ducimus.</p>
+	   <p class="card_p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus amet architecto vel qui, voluptas beatae,
+			veritatis officiis hic animi, sint itaque!</p>
+   </div>
+   </div>`
+	}
+	function btn2(){
+		document.querySelector("#c1").style="background: white;"
+		document.querySelector("#c2").style="background: rgb(0, 195, 255);"
+		document.querySelector("#c3").style="background: white;"
+		document.querySelector("#c4").style="background: white;"
+		document.querySelector('.card_text').innerHTML=`  
+		 <div class="card_text" id="text1">
+		<h2 class="title">  Lorem ipsum dolor sit amet.</h2>
+	 <div class="suptitle">
+	   <div class="second">
+		   <div class="cub" ><div class="span"></div> Lorem ipsum dolor sit.</div>
+		   <div class="cub" ><div class="span"></div>  Molestias magnam</div>
+		   <div class="cub" ><div class="span"></div>  tempora illum porro </div>
+	   </div>
+	   <div class="first">
+		   <div class="cub" ><div class="span"></div> Atque nulla delectus</div>
+		   <div class="cub" ><div class="span"></div>  neque accusamus fugiat</div>
+		   <div class="cub" ><div class="span"></div>  sequi unde aspernatur</div>
+	   </div>
+   
+   </div>
+   <div>
+	   <p class="card_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error temporibus voluptates alias pariatur minima,
+			fugiat suscipit eum quaerat tempora earum doloribus qui itaque deleniti ex blanditiis assumenda vel! Sed, ducimus.</p>
+	   <p class="card_p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus amet architecto vel qui, voluptas beatae,
+			veritatis officiis hic animi, sint itaque!</p>
+   </div>
+   </div>`
+	}
+	function btn3(){
+		document.querySelector("#c1").style="background: white;"
+		document.querySelector("#c2").style="background: white;"
+		document.querySelector("#c3").style="background: rgb(0, 195, 255);"
+		document.querySelector("#c4").style="background: white;"
+		document.querySelector('.card_text').innerHTML=`	
+		 <div class="card_text" id="text1">
+		<h2 class="title">Aliquam ullam nostrum magni</h2>
+	 <div class="suptitle">
+	   <div class="second">
+		   <div class="cub" ><div class="span"></div>Aut quis inventore</div>
+		   <div class="cub" ><div class="span"></div> expedita molestiae</div>
+		   <div class="cub" ><div class="span"></div> incidunt eligendi</div>
+	   </div>
+	   <div class="first">
+		   <div class="cub" ><div class="span"></div>tenetur quae veniam</div>
+		   <div class="cub" ><div class="span"></div>autem eveniet laboriosam</div>
+		   <div class="cub" ><div class="span"></div> inciduntautem eveniet labori</div>
+	   </div>
+   
+   </div>
+   <div>
+	   <p class="card_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error temporibus voluptates alias pariatur minima,
+			fugiat suscipit eum quaerat tempora earum doloribus qui itaque deleniti ex blanditiis assumenda vel! Sed, ducimus.</p>
+	   <p class="card_p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus amet architecto vel qui, voluptas beatae,
+			veritatis officiis hic animi, sint itaque!</p>
+   </div>
+   </div>`
+	}
+	function btn4(){
+		document.querySelector("#c1").style="background: white;"
+		document.querySelector("#c2").style="background: white;"
+		document.querySelector("#c3").style="background: white;"
+		document.querySelector("#c4").style="background: rgb(0, 195, 255);"
+		document.querySelector('.card_text').innerHTML=`	 <div class="card_text" id="text1">
+		<h2 class="title">Tempora illum porro ducimus</h2>
+	 <div class="suptitle">
+	   <div class="second">
+		   <div class="cub" ><div class="span"></div>quae veniam nesciunt </div>
+		   <div class="cub" ><div class="span"></div>consectetur doloribus</div>
+		   <div class="cub" ><div class="span"></div> ducimus laboriosam velit </div>
+	   </div>
+	   <div class="first">
+		   <div class="cub" ><div class="span"></div> aliquam ullam nostrum</div>
+		   <div class="cub" ><div class="span"></div> exercitationem atque</div>
+		   <div class="cub" ><div class="span"></div>dolores reprehenderit ipsa </div>
+	   </div>
+   
+   </div>
+   <div>
+	   <p class="card_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error temporibus voluptates alias pariatur minima,
+			fugiat suscipit eum quaerat tempora earum doloribus qui itaque deleniti ex blanditiis assumenda vel! Sed, ducimus.</p>
+	   <p class="card_p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus amet architecto vel qui, voluptas beatae,
+			veritatis officiis hic animi, sint itaque!</p>
+   </div>
+   </div>`
+	}
+	
